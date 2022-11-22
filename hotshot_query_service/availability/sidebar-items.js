423f7537bb670d215b51f24d13a3f338f4408b5b
jbearer <jbearer@users.noreply.github.com>
2022-11-22 19:35:14 +0000

    deploy: 7b80c881d318639b7b100f4a7b99aefbe7bf9bba

--- hotshot_query_service/availability/sidebar-items.js
+++ hotshot_query_service/availability/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["Error",""]],"fn":[["define_api",""]],"struct":[["Options",""]]};
+window.SIDEBAR_ITEMS = {"enum":[["Error",""]],"fn":[["define_api",""]],"struct":[["BlockQueryData",""],["LeafQueryData",""],["Options",""],["TransactionQueryData",""]],"trait":[["AvailabilityDataSource",""]],"type":[["BlockHash",""],["LeafHash",""],["TransactionHash",""]]};
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"hotshot_query_service":["",[],["availability.rs","data_source.rs","lib.rs","status.rs"]]\
+"hotshot_query_service":["",[["availability",[],["data_source.rs","query_data.rs"]]],["availability.rs","data_source.rs","lib.rs","status.rs"]]\
 }');
 createSourceSidebar();
