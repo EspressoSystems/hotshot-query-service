98ba2b923d09a1f67a7d373735f4fb0346b59707
jbearer <jbearer@users.noreply.github.com>
2023-10-12 14:58:52 +0000

    deploy: edbb0857e7595312dd042d130f6f21cbaf8a761c

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"hotshot_query_service/status/struct.MempoolQueryData.html\" title=\"struct hotshot_query_service::status::MempoolQueryData\">MempoolQueryData</a>"]]
+"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"hotshot_query_service/status/struct.MempoolQueryData.html\" title=\"struct hotshot_query_service::status::MempoolQueryData\">MempoolQueryData</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "hotshot_query_service":["",[["availability",[],["data_source.rs","query_data.rs"]],["status",[],["data_source.rs","query_data.rs"]]],["api.rs","availability.rs","data_source.rs","error.rs","ledger_log.rs","lib.rs","metrics.rs","resolvable.rs","status.rs","update.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
