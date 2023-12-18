008d8c82734a7973c4a9c041758962169f10d2aa
jbearer <jbearer@users.noreply.github.com>
2023-12-18 17:23:32 +0000

    deploy: 4c7cb32c00f789958a32217fdd866c860ceea064

---
+++ implementors/tide_disco/metrics/trait.Metrics.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hotshot_query_service":[["impl Metrics for <a class=\"struct\" href=\"hotshot_query_service/metrics/struct.PrometheusMetrics.html\" title=\"struct hotshot_query_service::metrics::PrometheusMetrics\">PrometheusMetrics</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
