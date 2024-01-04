8ae9b51cd9561e97c5e8bd486780b897315c5eb6
jbearer <jbearer@users.noreply.github.com>
2024-01-04 19:09:09 +0000

    deploy: 2f26181bda76ea947e768d3f24eba3cf7662d658

---
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"hotshot_query_service/data_source/sql/struct.Config.html\" title=\"struct hotshot_query_service::data_source::sql::Config\">Config</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
