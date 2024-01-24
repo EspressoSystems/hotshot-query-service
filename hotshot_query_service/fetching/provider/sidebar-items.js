4a9f5d3af449588d625ca1f1056274a693acd56b
jbearer <jbearer@users.noreply.github.com>
2024-01-24 16:53:37 +0000

    deploy: a9a094a93f65fc4c05f50268b07511cb0e5879d5

---
+++ hotshot_query_service/fetching/provider/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["NoFetching","QueryServiceProvider"],"trait":["LocalProvider","Provider"]};
---
+++ hotshot_query_service/fetching/request/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["LeafRequest","PayloadRequest"],"trait":["Request"]};
