11f92732373b31e7a5437fd0ff60fc8bef5f4ca3
tbro <tbro@users.noreply.github.com>
2024-05-30 14:04:53 -0400

    Fix: TmpDb::init() wait for psql
    Currently `TmpDb::init()` attempts to wait for postgres to become ready,
    but the logic will panic if `psql` is not available on the
    host. But, we should not have any expectation that `psql` be available
    on the host. This PR updates the wait loop to run a query w/ `psql` inside
    the docker container.
    
    NOTE due to incoming hotshot and sequencer changes this hasn't been properly tested so there
    may be some bugs. Once hotshot is updated we can test this properly.

--- src/data_source/storage/sql.rs
+++ src/data_source/storage/sql.rs
@@ -3101,27 +3101,33 @@ pub mod testing {
             };
 
             // Wait for the database to be ready.
-            while !Command::new("psql")
-                .args([
-                    "-h",
-                    &(db.host()),
-                    "-p",
-                    &(db.port().to_string()),
-                    "-U",
-                    "postgres",
-                ])
-                .env("PGPASSWORD", "password")
-                // Null input so the command terminates as soon as it manages to connect.
-                .stdin(Stdio::null())
-                // Output from this command is not useful, it's just a prompt.
-                .stdout(Stdio::null())
-                .stderr(Stdio::null())
-                .status()
-                .unwrap()
-                .success()
-            {
-                tracing::warn!("database is not ready");
-                sleep(Duration::from_secs(1)).await;
+            loop {
+                if Command::new("docker")
+                    .args([
+                        "exec",
+                        "-h",
+                        &(db.host()),
+                        "-p",
+                        &(db.port().to_string()),
+                        "-U",
+                        "postgres",
+                        "psql",
+                    ])
+                    .env("PGPASSWORD", "password")
+                    // Null input so the command terminates as soon as it manages to connect.
+                    .stdin(Stdio::null())
+                    // Output from this command is not useful, it's just a prompt.
+                    .stdout(Stdio::null())
+                    .stderr(Stdio::null())
+                    .status()
+                    .map(|status| status.success())
+                    .is_ok()
+                {
+                    break;
+                } else {
+                    tracing::warn!("database is not ready");
+                    sleep(Duration::from_secs(1)).await;
+                }
             }
 
             db
