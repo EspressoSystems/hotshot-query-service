5517a0655a3f3ba29955f05cdce0f0ac1dddf68d
imabdulbasit <imabdulbasit@users.noreply.github.com>
2024-06-21 17:50:46 +0000

    deploy: 4f09c49835031dbbfb2d741388398df872d4f5c4

--- trait.impl/core/ops/arith/trait.Sub.js
+++ trait.impl/core/ops/arith/trait.Sub.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"hotshot_query_service/explorer/struct.MonetaryValue.html\" title=\"struct hotshot_query_service::explorer::MonetaryValue\">MonetaryValue</a>"]]
+"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"hotshot_query_service/explorer/struct.MonetaryValue.html\" title=\"struct hotshot_query_service::explorer::MonetaryValue\">MonetaryValue</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- trait.impl/core/str/traits/trait.FromStr.js
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.78.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"hotshot_query_service/data_source/storage/sql/struct.Config.html\" title=\"struct hotshot_query_service::data_source::storage::sql::Config\">Config</a>"]]
+"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"hotshot_query_service/data_source/storage/sql/struct.Config.html\" title=\"struct hotshot_query_service::data_source::storage::sql::Config\">Config</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
