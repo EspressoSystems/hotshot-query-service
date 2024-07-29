6864fc553521534a864fcf8505cd265eff063b36
Jeb Bearer <jeb@espressosys.com>
2024-07-29 10:09:38 -0400

    Refactor database transactions
    A DB transaction is a proper type now in its own right, as opposed to having
    transaction logic (commit, revert) as functions on the main server object, which
    lead to some horrific ownership semantics and footguns (like the need to
    explicitly revert a transaction if it failed, or else all future modifications
    would use the same aborted transaction and also fail). The new design is much
    cleaner and also a better match for SQLx's ownership semantics. From here on
    out, actually switching from tokio-postgres to sqlx should be straightforward.
    
    One immediate consequence of this change is that all mutable operations now
    happen on the transaction, rather than the data source itself, so we no longer
    need to acquire a write lock to mutate the database. This should be a huge
    performance win as it allows reads to proceed while an update is in flight,
    avoiding long queues on the RwLock. It also allows concurrent writers to the
    extent that the underlying database does. Of course, we still have a single
    connection, so we won't see the benefits of this immediately, but the benefits
    will be huge once we switch to sqlx and use their connection pool.

--- src/data_source/fs.rs
+++ src/data_source/fs.rs
@@ -159,7 +159,7 @@ where
     ///
     /// The [FileSystemDataSource] will manage its own persistence synchronization.
     pub async fn create(path: &Path, provider: P) -> anyhow::Result<Self> {
-        FetchingDataSource::builder(FileSystemStorage::create(path).await?, provider)
+        FileSystemDataSource::builder(FileSystemStorage::create(path).await?, provider)
             .build()
             .await
     }
@@ -170,7 +170,7 @@ where
     ///
     /// The [FileSystemDataSource] will manage its own persistence synchronization.
     pub async fn open(path: &Path, provider: P) -> anyhow::Result<Self> {
-        FetchingDataSource::builder(FileSystemStorage::open(path).await?, provider)
+        FileSystemDataSource::builder(FileSystemStorage::open(path).await?, provider)
             .build()
             .await
     }
@@ -187,7 +187,7 @@ where
         loader: &mut AtomicStoreLoader,
         provider: P,
     ) -> anyhow::Result<Self> {
-        FetchingDataSource::builder(
+        FileSystemDataSource::builder(
             FileSystemStorage::create_with_store(loader).await?,
             provider,
         )
@@ -207,7 +207,7 @@ where
         loader: &mut AtomicStoreLoader,
         provider: P,
     ) -> anyhow::Result<Self> {
-        FetchingDataSource::builder(FileSystemStorage::open_with_store(loader).await?, provider)
+        FileSystemDataSource::builder(FileSystemStorage::open_with_store(loader).await?, provider)
             .build()
             .await
     }
@@ -222,8 +222,8 @@ where
     /// this [FileSystemDataSource] must be advanced, either by
     /// [commit](super::VersionedDataSource::commit) or, if there are no outstanding changes,
     /// [skip_version](Self::skip_version).
-    pub async fn skip_version(&mut self) -> anyhow::Result<()> {
-        self.storage_mut().await.skip_version()?;
+    pub async fn skip_version(&self) -> anyhow::Result<()> {
+        self.as_ref().skip_version().await?;
         Ok(())
     }
 }
@@ -232,7 +232,7 @@ where
 mod impl_testable_data_source {
     use super::*;
     use crate::{
-        data_source::{UpdateDataSource, VersionedDataSource},
+        data_source::{Transaction, UpdateDataSource, VersionedDataSource},
         testing::{consensus::DataSourceLifeCycle, mocks::MockTypes},
     };
     use async_trait::async_trait;
@@ -262,8 +262,9 @@ mod impl_testable_data_source {
         }
 
         async fn handle_event(&mut self, event: &Event<MockTypes>) {
-            self.update(event).await.unwrap();
-            self.commit().await.unwrap();
+            let mut tx = self.transaction().await.unwrap();
+            tx.update(event).await.unwrap();
+            tx.commit().await.unwrap();
         }
     }
 }
