1983c6b9f320fda6499938daeb43b11c2e2940da
rob-maron <rob-maron@users.noreply.github.com>
2024-08-20 14:05:59 +0000

    deploy: 316258fdcd8c495bd4cba41cde9c7c84abeb9de2

---
+++ trait.impl/hotshot_types/traits/node_implementation/trait.Versions.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hotshot_query_service":[["impl Versions for <a class=\"struct\" href=\"hotshot_query_service/testing/mocks/struct.MockVersions.html\" title=\"struct hotshot_query_service::testing::mocks::MockVersions\">MockVersions</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
