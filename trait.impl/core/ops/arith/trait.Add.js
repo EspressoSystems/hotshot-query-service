1077eb07739bfa8c22150563bb6ceea9c0bfb178
jparr721 <jparr721@users.noreply.github.com>
2024-09-09 14:47:22 +0000

    deploy: 90e628fb7043608731e2e23c32ad6dee3c090602

--- trait.impl/core/ops/arith/trait.Add.js
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for <a class=\"struct\" href=\"hotshot_query_service/explorer/struct.MonetaryValue.html\" title=\"struct hotshot_query_service::explorer::MonetaryValue\">MonetaryValue</a>"]]
+"hotshot_query_service":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for <a class=\"struct\" href=\"hotshot_query_service/explorer/struct.MonetaryValue.html\" title=\"struct hotshot_query_service::explorer::MonetaryValue\">MonetaryValue</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
