7a9c41f9131862e4ec03b3efb03fd5ebe232bbe3
jbearer <jbearer@users.noreply.github.com>
2024-10-29 19:50:04 +0000

    deploy: d062dc78910700005962c27441c19489200a3ac8

--- hotshot_query_service/data_source/sql/sidebar-items.js
+++ hotshot_query_service/data_source/sql/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":["include_migrations"],"mod":["testing"],"struct":["Error","Migration","Postgres"],"type":["Builder","Config","SqlDataSource"]};
+window.SIDEBAR_ITEMS = {"macro":["include_migrations"],"mod":["testing"],"struct":["Error","Migration"],"type":["Builder","SqlDataSource"]};
