0a31206a338dce03adf4569d81f680ed058f46db
jbearer <jbearer@users.noreply.github.com>
2024-11-19 20:07:17 +0000

    deploy: 7074bee49d7377abaa9e23d46b1622472c84247d

--- hotshot_query_service/node/sidebar-items.js
+++ hotshot_query_service/node/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Error","WindowStart"],"fn":["define_api"],"struct":["CustomSnafu","Options","QuerySnafu","QueryVidSnafu","QueryWindowSnafu","RequestSnafu","SyncStatus","TimeWindowQueryData"],"trait":["NodeDataSource"]};
+window.SIDEBAR_ITEMS = {"enum":["Error","WindowStart"],"fn":["define_api"],"struct":["CustomSnafu","Limits","Options","QuerySnafu","QueryVidSnafu","QueryWindowSnafu","RequestSnafu","SyncStatus","TimeWindowQueryData"],"trait":["NodeDataSource"]};
